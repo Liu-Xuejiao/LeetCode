43dd3ea91c70ff2a37732964078d6577e8c223ad
xliu2 <xliu2@ie.ibm.com>
2018-05-14 16:08:48 +0100

    Question 1 solution.

---
+++ Question_1.java
@@ -0,0 +1,24 @@
+import java.util.Vector;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        	int answer[] = new int[2];
+        Vector v = new Vector();
+        for(int i : nums){
+        	v.add(i);
+        }
+        for(int i=0;i<nums.length;i++){
+        	int c = target - nums[i];
+        	if(v.contains(c)){
+        		int index = v.indexOf(c);
+        		if(index != i){
+        			answer[1] = i;
+            		answer[0] = index;
+                    break;
+        		}
+                
+        	}
+        }
+        return answer;
+    }
+}
