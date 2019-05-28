a6ffdf6c66a6ff0d3a35dfe83990d24b47ef14a2
xuejiaoliu <xuejiaoliu@xuejiaos-mbp.mul.ie.ibm.com>
2019-05-28 16:06:20 +0100

    Question_268

--- Question_268.java
+++ Question_268.java
@@ -5,16 +5,29 @@ class Solution {
         for(int i=0; i<size; i++){
             arrli.add((Integer)i);
         }
-        
-        
         for(int num : nums){
             arrli.remove((Integer)num);
         }
-        
         if(!arrli.isEmpty()){
             return arrli.get(0);
         }
-        
         return size;
     }
 }
+
+
+/*
+class Solution {
+    public int missingNumber(int[] nums) {
+        int size = nums.length;
+        
+        int result = (size+1) * size / 2;
+        
+        for (int num : nums){
+            result -= num;
+        }
+        
+        return result;
+    }
+}
+*/
