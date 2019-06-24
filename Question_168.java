4faf3657b7eeb5421eb3733347f93b832251220b
xuejiaoliu <xuejiaoliu@localhost>
2019-06-24 17:06:02 +0100

    Question_168

--- Question_168.java
+++ Question_168.java
@@ -1,20 +1,20 @@
 class Solution {
 	public String convertToTitle(int n) {
 		int Ascii = 64;
-		int size = 26;
 		String result = "";
 		int index = 0;
 		while (n > 0) {
 			if (n <= 26) {
 				result = result + (char) (n + Ascii);
-				n = 0;
+				break;
 			}
-			
-			if( n%26 == 0) {
-				size = 27*(n/26-1);
+
+			index = n / 26;
+
+			if (n % 26 == 0) {
+				index = index - 1;
 			}
-			
-			index = n / size;
+
 			result = result + convertToTitle(index);
 			n = n - index * 26;
 		}
